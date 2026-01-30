c1fdd64a79893542100a3abef30b77e42c484d27
Rachakulla_SivaRamaKrishna <125557420+Rsivaramakrishna@users.noreply.github.com>
2026-01-30 20:05:11 +0530

    Create Random_module.py for random integer generation
    Added a new Random_module.py demonstrating the use of the random module to generate a random integer.

---
+++ Miscellaneous /python_standard_library /Random_module.py
@@ -0,0 +1,11 @@
+#Randomness is useful in whenever uncertainty is required.  
+#For example: Rolling a dice, flipping a coin, etc., 
+#random  module provides us utilities to create randomness.
+
+#randint() is a function in random module which returns a random integer in the given interval.
+
+import random
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#output: 8
