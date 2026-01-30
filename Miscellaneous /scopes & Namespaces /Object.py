8d90c7935c066d86e6c6aae3ef6471d742a0698a
Rachakulla_SivaRamaKrishna <125557420+Rsivaramakrishna@users.noreply.github.com>
2026-01-30 20:34:41 +0530

    Add unique identifiers to variable assignments
    Updated variable assignments with unique identifiers.

---
+++ Miscellaneous /scopes & Namespaces /Object.py
@@ -0,0 +1,19 @@
+#OBJECT
+#In general, anything that can be assigned to a variable in Python is referred to as an object.
+#Unique identifier(id)
+#Whenever an object is created in Python, it will be given a unique identifier (id).This unique id can be different for each time you run the program
+
+a = "Hello" # id-2646256526651
+n = "world" # id-5524765145451
+
+
+
+
+
+
+
+
+
+
+
+
